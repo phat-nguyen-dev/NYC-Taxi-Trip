42534503634d4b29c5f7122e7abf205a6ff109fb
Phat Nguyen <phatnguyentan990@gmail.com>
2026-03-17 22:24:25 +0700

    Init logger library

---
+++ scripts/common/logger.py
@@ -0,0 +1,27 @@
+import datetime as dt
+ 
+VALID_LEVELS = {"INFO", "WARNING", "ERROR", "DEBUG"}
+file_path = "logs"
+
+def write_log(message, file_name, job=None, level='INFO'):
+    # Check level if valid
+    level = level.upper()
+    if level not in VALID_LEVELS:
+        message = f"Invalid level: ({level}) {message}"
+        level = "ERROR"
+
+    timestamp = dt.datetime.now()
+    current_date = timestamp.strftime("%Y-%m-%d")
+    current_timestamp = timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Message to write file and print terminal:
+    if job:
+        mes = f"{current_timestamp} | {job} | {level} | {message}"
+    else:
+        mes = f"{current_timestamp} | {level} | {message}"
+
+    # Write to log file and terminal
+    with open(f"{file_path}/{file_name}-{current_date}.log", "a", encoding="utf-8") as f:
+        f.write(f"{mes}\n")
+
+    print(mes)
